c98df07ca6a2eacfc8bcdd37dfeacc916932131f
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2020-11-08 08:08:02 -0500

    CRUD Operations done for products

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;//this comes form the DataAccess.InMemory is the CRUD operations in cache
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Product productToEdit = context.Find(Id);
+            if (productToEdit != null)
+            {
+
+                return View(productToEdit);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, String Id)
+        {
+            Product productToEdit = context.Find(Id);
+            if (productToEdit != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    productToEdit.Category = product.Category;
+                    productToEdit.Description = product.Description;
+                    productToEdit.Image = product.Image;
+                    productToEdit.Name = product.Name;
+                    productToEdit.Price = product.Price;
+
+                    context.Commit();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(product);
+                }
+          
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+
+
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete != null)
+            {
+
+                return View(productToDelete);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete != null)
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
